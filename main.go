7d064d862b4730f978f79568fbd3ae8c11f1b173
John Hamm <60947846+gohammdata@users.noreply.github.com>
2020-02-12 19:25:05 -0500

    Create main.go
    Setting up connection to SQL database and setting up the index function for the database records with http requests.

---
+++ main.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+    "database/sql"
+    "log"
+    "net/http"
+    "text/template"
+    
+    "github.com/go-sql-driver/mysql"
+    )
+
+type Employee struct {
+    Id int
+    Name string
+    City string
+}
+
+func dbConn() (db *sql.DB) {
+    dbDriver := "mysql"
+    dbUser := "root"
+    dbPass := "root"
+    dbName := "goblog"
+    db, err := sql.Open(dbDriver, dbUser+":"+dbPass+"@/"+dbName)
+    if err != nil {
+        panic(err.Error())
+    }
+    return db
+}
+
+var tmpl = template.Must(template.ParseGlob("form/*"))
+
+func Index(w http.ResponseWriter, r *http.Request) {
+    db := dbConn()
+    selDB, err := db.Query("SELECT * FROM Employee ORDER BY id DESC")
+    if err != nil {
+        panic(err.Error())
+    }
+    emp := Employee{}
+    res := []Employee{}
+    for selDB.Next() {
+        var id int
+        var name, city string
+        err = selDB.Scan(&id, &name, &city)
+        if err != nil {
+            panic(err.Error())
+        }
+        emp.ID = id
+        emp.Name = name
+        emp.City = city
+        res = append(res, emp)
+    }
+    tmpl.ExecuteTemplate(w, "Index", res)
+    defer db.Close()
+}
