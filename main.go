9e25d4408101b6cc48a4c466cfb3709b08e62a80
John Hamm <60947846+gohammdata@users.noreply.github.com>
2020-02-15 08:25:55 -0500

    rooot
    root

--- main.go
+++ main.go
@@ -18,7 +18,7 @@ type Employee struct {
 func dbConn() (db *sql.DB) {
     dbDriver := "mysql"
     dbUser := "root"
-    dbPass := "Tobi@s0506"
+    dbPass := "root"
     dbName := "goblog"
     db, err := sql.Open(dbDriver, dbUser+":"+dbPass+"@/"+dbName)
     if err != nil {
